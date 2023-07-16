1741a549fd1e326630523312e91f5b13934c7f4b
Ravi Sagar <ravi@sparxsys.com>
2023-07-16 17:43:33 +0100

    Application roles

---
+++ getAllApplicationRoles.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+curl -s \
+     GET -H "Authorization:Basic $JIRATUTORIAL_AUTH" \
+     "https://jiratutorial.atlassian.net/rest/api/3/applicationrole" \
+     | jq '.[] | "\(.key)" '
